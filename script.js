c4f3129456b5d32099fe7a16e2c5ff4d9700b6b4
Tay Araujo <132668816+TaizaArauj0@users.noreply.github.com>
2023-07-07 12:13:55 +0000

    tAIZA

--- script.js
+++ script.js
@@ -4,30 +4,29 @@ function contar() {
     let passo = document.getElementByInd('txtp')
     let res = document.getElementById(res)
 
-    if (ini.valeu.length == 0 || fim.valeu.length == 0 || passo.valeu.length == 0) {
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = 'Impossivel contar!'
        // window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
-        let i = Number(ini.valeu)
-        let f = Number(fim.valeu)
-        let p = Number(passo.valeu)
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
         if (p <= 0) { 
-            window.alert('Passo invalido!')
+            window.alert('Passo invalido! Considerando PASSO 1')
             p = 1
         }    
         if (i < f) {
             // Contagem crescente
           for(let c = i; c <= f; c += p) {
-            res.innerHTML += ' ${c} \u{1F449}'
+            res.innerHTML += `${c} \u{1F449}`
           }
         } else {
             // Contagem regressiva
             for(let c = i; c <= f; c += p) {
-                res.innerHTML += ' ${c} \u{1F449}'
+                res.innerHTML += `${c} \u{1F449}` 
+            }
         }
-    }
-    res.innerHTML += ' ${c} \u{1F3C1}'
-        
+        res.innerHTML += `\u{1F3C1}`        
     }
 }
