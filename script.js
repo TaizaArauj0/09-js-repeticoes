a10fe809e39130f43844ef700a2c6755d1b8b1c6
Tay Araujo <132668816+TaizaArauj0@users.noreply.github.com>
2023-07-07 15:52:28 +0000

    tay

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossivel contar!' 
+        res.innerHTML = 'Impossivel contar!'
         //window.alert('[ERRO] Faltam dados!')
     } else {
         res.innerHTML = 'Contando: <br>'
@@ -18,15 +18,15 @@ function contar() {
         }    
         if (i < f) {
             // Contagem crescente
-          for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} \u{1F449}`
-          }
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
         } else {
             // Contagem regressiva
             for(let c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{1F449}` 
+                res.innerHTML += `${c} \u{1F499}`
             }
         }
-        res.innerHTML += `\u{1F3C1}`        
+        res.innerHTML += `\u{1F3C1}`       
     }
 }
